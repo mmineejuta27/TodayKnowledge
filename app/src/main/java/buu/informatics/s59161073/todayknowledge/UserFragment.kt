6a458f182e56be58c1d25bf291fc351009578a11
mmineejuta27 <minejutamas@gmail.com>
2019-10-15 20:49:58 +0700

    add fragment user

---
+++ app/src/main/java/buu/informatics/s59161073/todayknowledge/UserFragment.kt
@@ -0,0 +1,28 @@
+package buu.informatics.s59161073.todayknowledge
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import buu.informatics.s59161073.todayknowledge.databinding.FragmentUserBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class UserFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding = DataBindingUtil.inflate<FragmentUserBinding>(inflater,
+            R.layout.fragment_user,container,false)
+        return binding.root
+    }
+
+
+}
