9f2b574c11c2231199b6437eea82a0aa991cb2a7
mmineejuta27 <minejutamas@gmail.com>
2019-10-15 20:22:24 +0700

    first app

---
+++ app/src/main/java/buu/informatics/s59161073/todayknowledge/MainActivity.kt
@@ -0,0 +1,15 @@
+package buu.informatics.s59161073.todayknowledge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import buu.informatics.s59161073.todayknowledge.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+}
