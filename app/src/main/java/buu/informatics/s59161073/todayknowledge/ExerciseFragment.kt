b84423aaf75241ee1fe24c63f66c2638b2336d61
mmineejuta27 <minejutamas@gmail.com>
2019-10-31 18:28:28 +0700

    add random question

--- app/src/main/java/buu/informatics/s59161073/todayknowledge/ExerciseFragment.kt
+++ app/src/main/java/buu/informatics/s59161073/todayknowledge/ExerciseFragment.kt
@@ -20,13 +20,13 @@ import org.w3c.dom.Text
  * A simple [Fragment] subclass.
  */
 class ExerciseFragment : Fragment() {
-    var a = 0
+    var checkValue = 0
 
     data class Exercise(
         val text: String,
         val images: List<Int>
     )
-    private val exercises1: MutableList<Exercise> = mutableListOf(
+    private val exercises2: MutableList<Exercise> = mutableListOf(
             Exercise(text = "waterMelon",
                 images = listOf(R.drawable.watermelon, R.drawable.banana, R.drawable.pumpkin, R.drawable.apple)),
             Exercise(text = "apple",
@@ -38,15 +38,36 @@ class ExerciseFragment : Fragment() {
 
     )
 
-    private val exercises2: MutableList<Exercise> = mutableListOf(
+    private val exercises1: MutableList<Exercise> = mutableListOf(
         Exercise(text = "Teacher",
             images = listOf(R.drawable.teacher, R.drawable.doctor, R.drawable.policeman, R.drawable.nurse)),
         Exercise(text = "Doctor",
             images = listOf(R.drawable.doctor, R.drawable.policeman, R.drawable.nurse, R.drawable.teacher)),
         Exercise(text = "Nurse",
-            images = listOf(R.drawable.nurse, R.drawable.policeman, R.drawable.nurse, R.drawable.doctor)),
+            images = listOf(R.drawable.nurse, R.drawable.policeman, R.drawable.teacher, R.drawable.doctor)),
         Exercise(text = "Policeman",
             images = listOf(R.drawable.policeman, R.drawable.nurse, R.drawable.teacher, R.drawable.doctor))
+    )
+
+    private val exercises3: MutableList<Exercise> = mutableListOf(
+        Exercise(text = "Bird",
+            images = listOf(R.drawable.bird, R.drawable.clownfish, R.drawable.tiger, R.drawable.duckling)),
+        Exercise(text = "Fish",
+            images = listOf(R.drawable.clownfish, R.drawable.bird, R.drawable.duckling, R.drawable.tiger)),
+        Exercise(text = "Duck",
+            images = listOf(R.drawable.duckling, R.drawable.clownfish, R.drawable.bird, R.drawable.tiger)),
+        Exercise(text = "Tiger",
+            images = listOf(R.drawable.tiger, R.drawable.clownfish, R.drawable.bird, R.drawable.duckling))
+    )
+    private val exercises4: MutableList<Exercise> = mutableListOf(
+        Exercise(text = "Red",
+            images = listOf(R.drawable.buttonred, R.drawable.buttonblue, R.drawable.buttongreen, R.drawable.buttonyellow)),
+        Exercise(text = "Yellow",
+            images = listOf(R.drawable.buttonyellow, R.drawable.buttonred, R.drawable.buttonblue, R.drawable.buttongreen)),
+        Exercise(text = "Blue",
+            images = listOf(R.drawable.buttonblue, R.drawable.buttonred, R.drawable.buttonyellow, R.drawable.buttongreen)),
+        Exercise(text = "Green",
+            images = listOf(R.drawable.buttongreen, R.drawable.buttonred, R.drawable.buttonblue, R.drawable.buttonyellow))
     )
 
     lateinit var currentExercise: Exercise
@@ -65,46 +86,103 @@ class ExerciseFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater,
             R.layout.fragment_exercise,container,false)
         var valueGroping = args.groupButton
-        if (valueGroping == 1){
-            exercises1
+
+        val exerciseResource = when (valueGroping) {
+            1 -> exercises1
+            2 -> exercises2
+            3 -> exercises3
+            else -> exercises4
         }
 
-        randomizeExercise()
+        randomizeExercise(args.groupButton)
 
         binding.ansButton1.setOnClickListener {
             view ->
-            if (questionIndex >= exercises1.size-1){
+            if (questionIndex >= exerciseResource.size-1){
                 view.findNavController().navigate(R.id.action_exerciseFragment_to_scoreFragment)
             }else{
+                checkAns(0)
                 questionIndex++
-                currentExercise = exercises1[questionIndex]
-                setQuestion()
+                currentExercise = exerciseResource[questionIndex]
+                setQuestion(args.groupButton)
                 binding.invalidateAll()
             }
         }
-        binding.ansButton2.setOnClickListener {
-                view -> view.findNavController().navigate(R.id.action_exerciseFragment_to_scoreFragment)
+        binding.ansButton2.setOnClickListener { view ->
+            if (questionIndex >= exerciseResource.size-1){
+                view.findNavController().navigate(R.id.action_exerciseFragment_to_scoreFragment)
+            }else{
+                checkAns(1)
+                questionIndex++
+                currentExercise = exerciseResource[questionIndex]
+                setQuestion(args.groupButton)
+                binding.invalidateAll()
+            }
         }
         binding.ansButton3.setOnClickListener {
-                view -> view.findNavController().navigate(R.id.action_exerciseFragment_to_scoreFragment)
+                view ->
+            if (questionIndex >= exerciseResource.size-1){
+                view.findNavController().navigate(R.id.action_exerciseFragment_to_scoreFragment)
+            }else{
+                checkAns(2)
+                questionIndex++
+                currentExercise = exerciseResource[questionIndex]
+                setQuestion(args.groupButton)
+                binding.invalidateAll()
+            }
         }
         binding.ansButton4.setOnClickListener {
-                view -> view.findNavController().navigate(R.id.action_exerciseFragment_to_scoreFragment)
+                view ->
+            if (questionIndex >= exerciseResource.size-1){
+                view.findNavController().navigate(R.id.action_exerciseFragment_to_scoreFragment)
+            }else{
+                checkAns(3)
+                questionIndex++
+                currentExercise = exerciseResource[questionIndex]
+                setQuestion(args.groupButton)
+                binding.invalidateAll()
+            }
         }
 
         return binding.root
     }
 
-    private fun randomizeExercise() {
-        exercises1.shuffle()
-        questionIndex = 0
-        setQuestion()
+    private fun checkAns(index:Int) {
+        if(answers[index] == currentExercise.images[0]){
+            checkValue++
+            Toast.makeText(context,"Correct : ${checkValue}",Toast.LENGTH_LONG).show()
+            Log.i("checlAns","${checkValue}")
+        }else{
+            Toast.makeText(context,"Not Correct : ${checkValue}",Toast.LENGTH_LONG).show()
+        }
     }
 
-    private fun setQuestion(){
-        val textQuestion : TextView = binding.exerciseText
+    private fun randomizeExercise(group: Int) {
+        if(group == 1){
+            exercises1.shuffle()
+        }else if (group == 2){
+            exercises2.shuffle()
+        }else if (group == 3){
+            exercises3.shuffle()
+        }else {
+            exercises4.shuffle()
+        }
+//        exercises1.shuffle()
+        questionIndex = 0
+        setQuestion(group)
+    }
 
-        currentExercise = exercises1[questionIndex]
+    private fun setQuestion(group: Int){
+        val textQuestion : TextView = binding.exerciseText
+        if(group == 1){
+            currentExercise = exercises1[questionIndex]
+        }else if (group == 2){
+            currentExercise = exercises2[questionIndex]
+        }else if (group == 3){
+            currentExercise = exercises3[questionIndex]
+        }else {
+            currentExercise = exercises4[questionIndex]
+        }
 
         answers = currentExercise.images.toMutableList()
         answers.shuffle()
